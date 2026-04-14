d212c5ffd3d597e9c1d137db1f541ce2d64dd43c
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-04-14 08:40:58 +0600

    replace those word

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
-import os
+# import os
+# import json
+# from pymongo import MongoClient
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# uri = os.environ.get("MONGO_URI")
+
+# collection = MongoClient(uri)['test']['examquestionbanks']
+
+# data = collection.find({}, {'question.explanation'}).to_list()
+
+# # print(list(data))
+
+
+
+# with open("explanation_data.json", 'w', encoding="utf-8") as f:
+#     json.dump(data, f, indent=4, default=str)
+
+import ast
 import json
-from pymongo import MongoClient
-from dotenv import load_dotenv
+from asset.core.clear_data import CleanData
+with open('explanation_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
-load_dotenv()
+data = str(data)
 
-uri = os.environ.get("MONGO_URI")
+clean_data = ast.literal_eval(data)
 
-collection = MongoClient(uri)['test']['examquestionbanks']
+with open("explanation_data_1.json", 'w', encoding="utf-8") as f:
+    json.dump(clean_data, f, indent=4, default=str)
 
-data = collection.find({}, {'question.explanation'}).to_list()
-
-print(list(data))
-
-
-
-with open("explanation_data.json", 'w', encoding="utf-8") as f:
-    json.dump(data, f, indent=4, default=str)
